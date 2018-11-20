74507b4a1baa163985edd4c606f81be49bae00de
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-20 08:43:50 -0500

    flattenDeep complete

--- server/funky-funcs/index.js
+++ server/funky-funcs/index.js
@@ -1,6 +1,28 @@
-const intersection = (arr1, arr2) => {};
+const intersection = (arr1, arr2) => {
+  const intersectionArray = [];
 
-const flattenDeep = arr => {};
+  arr1.forEach(element => {
+    const isDuplicate = intersectionArray.includes(element);
+    if (arr2.includes(element) && !isDuplicate) {
+      intersectionArray.push(element);
+    }
+  });
+
+  return intersectionArray;
+};
+
+const flattenDeep = (arr, flattenedArr = []) => {
+  const nextArr = arr.slice(1);
+
+  if (!arr.length) {
+    return flattenedArr;
+  } else if (Array.isArray(arr[0])) {
+    return flattenDeep(arr[0], flattenedArr).concat(flattenDeep(nextArr));
+  } else {
+    flattenedArr.push(arr[0]);
+    return flattenDeep(nextArr, flattenedArr);
+  }
+};
 
 const flipArguments = func => {};
 
