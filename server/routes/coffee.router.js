75f4df28d42bf7ff9c0479beb7e108623879c51d
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-19 23:18:10 -0500

    coffee routes complete

--- server/routes/coffee.router.js
+++ server/routes/coffee.router.js
@@ -5,4 +5,30 @@ const {Coffee} = require('../models')
 // Remember that these routes are already mounted on
 // /api/coffee!
 
+router.get('/', async (req, res, next) => {
+  res.status(200).send(await Coffee.findAll());
+});
+
+router.get('/:coffeeId', async (req, res, next) => {
+  const coffeeId = req.params.coffeeId;
+  const requestedCoffee = await Coffee.findById(coffeeId);
+
+  if (!requestedCoffee) {
+    res.status(404).send();
+  } else {
+    res.status(200).send(requestedCoffee);
+  }
+});
+
+router.get('/ingredients/:ingredientName', async (req, res, next) => {
+  const ingredientName = req.params.ingredientName;
+  res.status(200).send(await Coffee.findByIngredient(ingredientName));
+});
+
+router.post('/', async (req, res, next) => {
+  const newCoffee = await Coffee.create(req.body);
+
+  res.status(201).send(newCoffee);
+});
+
 module.exports = router
