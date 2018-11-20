598b287accbf019fde0cb66c8453016ae28b4f43
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-19 19:12:48 -0500

    defining models, associations set

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -1,8 +1,34 @@
-const Sequelize = require('sequelize')
-const db = require('./database')
+const Sequelize = require('sequelize');
+const db = require('./database');
+const Op = Sequelize.Op;
 
 const Coffee = db.define('coffee', {
   // your code here
-})
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  ingredients: Sequelize.ARRAY(Sequelize.STRING),
+});
 
-module.exports = Coffee
+Coffee.findByIngredient = function (ingredient) {
+  return Coffee.findAll({
+    where: {
+      ingredients: {
+        [Op.contains]: [ingredient]
+      }
+    }
+  });
+};
+
+Coffee.prototype.getIngredients = function () {
+  return this.ingredients.join(', ');
+};
+
+Coffee.beforeValidate(coffee => {
+  if (coffee.ingredients.indexOf('love') === -1) {
+    coffee.ingredients.push('love');
+  }
+});
+
+module.exports = Coffee;
--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,16 @@
-const db = require('./database')
-const Pug = require('./pug.model')
-const Coffee = require('./coffee.model')
+const db = require('./database');
+const Pug = require('./pug.model');
+const Coffee = require('./coffee.model');
 
 // VVV assign relations below VVV //
+Coffee.hasMany(Pug);
+Pug.belongsTo(Coffee, { as: 'favoriteCoffee' });
 
-
+Pug.hasMany(Pug, { as: 'friends' });
 // ^^^ assign relations above ^^^ //
 
 module.exports = {
   db,
   Pug,
-  Coffee
-}
+  Coffee,
+};
